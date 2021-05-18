c2b1eb3959b83bb9a916b6fa6de3b29e580693e7
mklobukowski <michal.klobukowski@synergycodes.com>
2021-05-18 21:04:21 +0200

    Add some mocked data to serve as a tutorial

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import { Elm } from "./Main.elm";
 
-const entries = JSON.parse(localStorage.getItem("entriesCollection") || "[]");
+const tutorialEntries =
+  '[{"id":"4","title":"1. Add a new lane using the button in the top right corner"},{"id":"5","title":"2. Rename the new lane to Done (Click on the name of the lane)"},{"id":"6","title":"3. Drag and drop entries number 1, 2 and 3 from the Todo lane to the Done lane"},{"id":"7","title":"4. Change order of entries in the Done lane by dragging and dropping them onto other entries"},{"id":"8","title":"5. Reload the application - you changes should be restored after the reload"}]';
+const entries = JSON.parse(
+  localStorage.getItem("entriesCollection") || tutorialEntries
+);
 
-const lanes = JSON.parse(localStorage.getItem("lanesCollection") || "[]");
+const tutorialLanes = '[{"id":"1","title":"Todo","entries":["4","5","6","7"]}]';
+const lanes = JSON.parse(
+  localStorage.getItem("lanesCollection") || tutorialLanes
+);
 
 const app = Elm.Main.init({
   node: document.querySelector("main"),
